1cd774a27a483d53181b54f3c0d8acac51d5a628
Konstantin Meixner <konstantin.meixner@freenet.de>
2023-05-03 17:06:45 +0200

    unidentifiedimageerror

--- ex02/load_image.py
+++ ex02/load_image.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import PIL
 
 
 def ft_load(path: str) -> np.ndarray:
     """ft_load(path: str) -> np.ndarray
 
-Load an image into an RGB array and returns it."""
+Load an image into an RGB array and returns it.
+Checks for valid path and if the file exists"""
     try:
         assert type(path) is str, "path must be a string"
         img_array = plt.imread(path)
@@ -15,5 +17,9 @@ Load an image into an RGB array and returns it."""
         quit()
     except FileNotFoundError:
         print("ft_load: FileNotFoundError: file %s not found" % path)
+        quit()
+    except PIL.UnidentifiedImageError:
+        print("ft_load: UnidentifiedImageError: %s\
+not a valid image or corrupted" % path)
         quit()
     return img_array
